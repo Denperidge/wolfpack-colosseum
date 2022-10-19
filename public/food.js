0cd71e41678e9476c546cbfb542498d9ed9adac4
Denperidge <denperidge@gmail.com>
2022-10-19 19:53:43 +0200

    Food can now be added

---
+++ public/food.js
@@ -0,0 +1,73 @@
+
+let database; let foods;
+let storage; let upload;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loadEl = document.querySelector('#load');
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.firestore().doc('/foo/bar').get().then(() => { });
+    // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    // firebase.analytics(); // call to activate
+    // firebase.analytics().logEvent('tutorial_completed');
+    // firebase.performance(); // call to activate
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    try {
+        let app = firebase.app();
+        let features = [
+            'auth',
+            'database',
+            'firestore',
+            'functions',
+            'messaging',
+            'storage',
+            'analytics',
+            'remoteConfig',
+            'performance',
+        ].filter(feature => typeof app[feature] === 'function');
+
+        storage = firebase.storage().ref();
+        form = document.getElementById("form");
+
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let name = form.name.value;
+    let description = form.description.value;
+
+    let newFile = storage.child(`food/${name}.jpeg`);
+    console.log(newFile)
+    newFile.put(form.file.files[0]).then((snapshot) => {
+        window.location.href = "/index.html?id=" + name;
+    })
+
+    
+    console.log("gsdf")
+});
+
+
+
+function NewComment(positive, comment) {
+
+    let newComment = comments.push();
+    newComment.set({
+        userId: "meow",
+        positive: positive,
+        comment: comment
+    });
+
+}
+
+window.NewComment = NewComment;
