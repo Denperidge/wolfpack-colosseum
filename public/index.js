223e31341ec78b1a16268b7db48a0e1061df20af
Denperidge <denperidge@gmail.com>
2022-10-19 19:08:23 +0200

    Initial Commit (basic comments)

---
+++ public/index.js
@@ -0,0 +1,82 @@
+
+let database; let comments;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loadEl = document.querySelector('#load');
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.firestore().doc('/foo/bar').get().then(() => { });
+    // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    // firebase.analytics(); // call to activate
+    // firebase.analytics().logEvent('tutorial_completed');
+    // firebase.performance(); // call to activate
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    try {
+        let app = firebase.app();
+        let features = [
+            'auth',
+            'database',
+            'firestore',
+            'functions',
+            'messaging',
+            'storage',
+            'analytics',
+            'remoteConfig',
+            'performance',
+        ].filter(feature => typeof app[feature] === 'function');
+
+        database = firebase.database();
+        comments = database.ref("comments");
+
+        RefreshComments();
+        comments.on("child_added", RefreshComments).on("child_changed", RefreshComments).on("child_removed", RefreshComments);
+
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+// Crude comment refresh
+function RefreshComments(data) {
+    let commentsHTML = "";
+    comments.once("value", (snapshot) => {
+
+        snapshot.forEach((item) => {
+            let value = item.val();
+            commentsHTML += `
+            <article>
+                <h2>${value.userId}</h2>
+                <p>${value.positive}</p>
+                <p>${value.comment}</p>
+            </article>
+            `;
+        });
+        document.getElementById("comments").innerHTML = commentsHTML;
+
+    }).then(() => {
+        
+    });
+
+    
+
+}
+
+function NewComment(positive, comment) {
+
+    let newComment = comments.push();
+    newComment.set({
+        userId: "meow",
+        positive: positive,
+        comment: comment
+    });
+
+}
+
+window.NewComment = NewComment;
